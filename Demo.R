831234249fee56ace435cb1821a3f58d6a6135ce
Sanket Panasuriya <spanasuriya892@rku.ac.in>
2021-01-05 08:38:30 +0530

    Demo program using factor

--- Demo.R
+++ Demo.R
@@ -1,3 +1,12 @@
 print("This file is created in RStudio")
 print("And now it's live on GitHub")
-print("_________________________")
+
+data <- c("East","West","East","North","North","East","West","West","West","East","North")
+
+print(data)
+print(is.factor(data))
+
+factor_data <- factor(data)
+
+print(factor_data)
+print(is.factor(factor_data))
